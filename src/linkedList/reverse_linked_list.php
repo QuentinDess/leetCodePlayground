96df94c4e4faa61aa7ae1ac1ee8de728c2f362da
quentinDess <quentin.dessartine@gmail.com>
2025-11-20 09:34:13 +0100

    feat: reversed list while create new one

--- src/linkedList/reverse_linked_list.php
+++ src/linkedList/reverse_linked_list.php
@@ -10,20 +10,46 @@ $linkedList->addAt('3', 2);
 $linkedList->addAt('4', 3);
 $linkedList->addAt('5', 4);
 
-/** Iterative O(n) and O(1)  */
-function reverse_linked_list(LinkedList $list): LinkedList{
+/** Iterative O(n) time and O(1)  space */
+function reverse_linked_list(LinkedList $list): void{
     $prev = null;
     $current = $list->head;
+    /** preventive when we have empty linked list or only a head */
+    if($current === null || $current->next === null) {
+        return;
+    }
     while($current !== null){
         $next = $current->next;
         $current->next = $prev;
         $prev = $current;
         $current = $next;
     }
-    return new LinkedList($prev);
+    $list->head = $prev;
 }
 
-$reversed = reverse_linked_list($linkedList);
+/** Iterative O(n) time and O(n) space */
+function recursive_linked_list_return_new_list(LinkedList $list): LinkedList{
+    $current = $list->head;
+    /** preventive when we have empty linked list or only a head */
+    if($current === null || $current->next === null) {
+        return $list;
+    }
+    $newList = new LinkedList();
+    while($current !== null){
+        $newList->addFirst($current->value);
+        $current = $current->next;
+    }
+    $list->head = $newList->head;
+    return $list;
+}
+
+reverse_linked_list($linkedList);
 echo PHP_EOL;
 
-$reversed->print();
+$linkedList->print();
+
+echo PHP_EOL;
+
+recursive_linked_list_return_new_list($linkedList);
+
+$linkedList->print();
